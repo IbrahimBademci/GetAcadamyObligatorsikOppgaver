e89d5782e9148278a4357333ea3e33e99838318c
Ibrahim <46523678+IbrahimBademci@users.noreply.github.com>
2019-08-05 10:14:45 +0200

    ObligatoriskOppgave1

---
+++ Mandatory_Assaignment1/Mandatory_Assaignment1/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Mandatory_Assaignment1
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int BirthYear { get; set; }
+        public string DeathYear { get; set; }
+        public Person Mother { get; set; }
+        public Person Father { get; set; }
+
+
+        public Person(int id)
+        {
+            Id = id;
+        }
+
+        public void Show(string firstWord)
+        {
+            var lastName = string.Empty;
+            var deathYear = string.Empty;
+
+            if (LastName != null)
+            {
+
+                lastName = $"/ Etter Navn:{LastName}";
+            }
+            else if (DeathYear != null)
+            {
+                deathYear = $"/ Dødsår: {DeathYear}";
+            }
+            Console.WriteLine($"{firstWord} {FirstName} / Født: {BirthYear} / ID:{Id} {lastName} {deathYear}");
+        }
+    }
+
+}
